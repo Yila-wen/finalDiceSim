dfbbeaaf1548fc7475d693558197de1621924a4c
yila-wen <jhuang4836@bths.edu>
2024-01-17 15:30:33 -0500

    Day3 WORK

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ const a = Math.floor((Math.random()*6)+1)
 }
 
 function temp(){
-    console.log(document.getElementById("diceNum").value)
-    console.log(document.getElementById("rollNum").value)
 
+    let tmp = Number(totalRoll-1)
+    
+
+    console.log(tmp)
     console.log(totalRoll)
 }
 
@@ -49,20 +51,29 @@ function roll(){
         }
     }
     console.log(totalSum + ":" + totalRoll)
-    document.getElementById("mean").innerHTML = Number(totalSum/totalRoll)
     
+    document.getElementById("mean").innerHTML = Number(totalSum/totalRoll)
+    document.getElementById("mode").innerHTML = modeFinder()
+    document.getElementById("median").innerHTML = medianFinder()
 }
 
 function rollOne(){
     let val = random();
     totalSum += Number(val)
+    singles[val-1] ++
     document.getElementById("dice2").src = "die" + val + ".png"
 }
 function rollTwo(){
     let val = random();
     let val2 = random();
-    totalSum += Number(val + val2)
-    console.log(totalSum)
+    const combination = Number(val + val2)
+    totalSum += combination
+    if (combination >6){
+        doubles[combination%7] ++
+    }
+    else{
+        singles[combination-1] ++
+    }
     document.getElementById("dice1").src = "die" + val + ".png" 
     document.getElementById("dice2").src = "die" + val2 + ".png"
 }
@@ -70,14 +81,76 @@ function rollThree(){
     let val = random();
     let val2 = random();
     let val3 = random();
-    totalSum += Number(val + val2 + val3)
-    console.log(totalSum)
+    const combination = Number(val + val2 + val3)
+    totalSum += combination
+    if (combination >12){
+        triples[combination%13] ++
+        console.log(triples)
+    }
+    else if(combination >6){
+        doubles[combination%7] ++
+    }
+    else{
+        singles[combination-1] ++
+    }
     document.getElementById("dice1").src = "die" + val + ".png"
     document.getElementById("dice2").src = "die" + val2 + ".png"
     document.getElementById("dice3").src = "die" + val3 + ".png"
     
 }
-function meanFinder(){
+function medianFinder(){
+let fullArray = singles.concat(doubles, triples)
 
+let middle = -1;
+let index = -1;
+let a = 0
+let midLocation = -1;
+if (totalRoll%2 == 1){
+    midLocation = (totalRoll-1)/2
+    for (let i = 0; i<18;i++){
+        if (a <=midLocation){
+            a += fullArray[i];
+            index = i;}
+    }
+    return index+1;
+}
+else {
+    midLocation = (totalRoll/2)-1
+    let checker = false;
+
+    for (let i = 0; i<18;i++){
+        if (a <=midLocation){
+            a += fullArray[i];
+            index = i;
+            console.log("INDEX: "+ index )
+
+            if (a >= midLocation){
+                return Number(((index+1)+(i+1))/2)
+
+            }
+        }
+        
+
+        
+    }
+
+    middle = (fullArray[totalRoll/2] + fullArray[(totalRoll/2)-1])/2
+}
+
+}
+function modeFinder(){
+let fullArray = singles.concat(doubles, triples)
+console.log(fullArray)
+let mode = -1;
+let highest = -1;
+let location = -1;
+for (let i = 0; i<18;i++){
     
+    if (highest < fullArray[i]){
+        highest = fullArray[i]
+        location = i;
+    }
+}
+return location+1;
+
 }
